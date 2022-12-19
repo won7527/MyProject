6caac74a3decaaa5c2e63173bfe919eb363ce741
won7527 <121008852+won7527@users.noreply.github.com>
2022-12-19 16:18:44 +0900

    Initial commit

---
+++ Source/MyProject/CodeTest.cpp
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CodeTest.h"
+
+#pragma warning (disabe: 4458) //c4456 경고끄기
+// Sets default values
+ACodeTest::ACodeTest()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void ACodeTest::BeginPlay()
+{
+	Super::BeginPlay();
+
+	//result = StringAppender(f_name, l_name);
+	//UE_LOG(LogTemp, Warning, TEXT("%s"), *result);
+	//
+	//int32 checker = 3;
+	//// 만일 checker에 들어있는 값이 짝수라면
+	//if (checker % 2 == 0)
+	//{
+	//	// "even" 이라고 출력한다.
+	//	UE_LOG(LogTemp, Warning, TEXT("even"))
+	//}
+	//// 그렇지 않고 checker에 들어있는 값이 홀수라면
+	//else
+	//{
+	//	// "odd" 라고 출력한다
+	//	UE_LOG(LogTemp, Warning, TEXT("odd"))
+	//}
+	// 
+	/* 7단의 값 출력
+	for (int32 i = 1; i < 10; i++)
+	{
+		int32 mult = 7 * i;
+		UE_LOG(LogTemp, Warning, TEXT("7 * %d = %d\n"), i, mult);
+	}*/
+}
+
+
+// Called every frame
+void ACodeTest::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
+FString ACodeTest::StringAppender(FString f, FString l)
+{
+	return f + l;
+}
---
+++ Source/MyProject/CodeTest.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CodeTest.generated.h"
+
+UCLASS()
+class MYPROJECT_API ACodeTest : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	ACodeTest();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+	FString f_name = "a";
+	FString l_name = "b";
+	FString result;
+
+	// 문자열 두개를 받아서 문자열 한개로 합쳐서 반환하는 함수를 만들고 싶다. 
+	UFUNCTION(BlueprintCallable)
+	FString StringAppender(FString f, FString s);
+
+};
